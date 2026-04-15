02e571d6920d327b72bcbe6cf299d58050b98a36
Wes <wesley7879@gmail.com>
2026-04-15 07:08:01 -0500

    Bump version to 1.9.9

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     setuptools.setup(
         name="graphreduce",
-        version = "1.9.8",
+        version = "1.9.9",
         url="https://github.com/wesmadrigal/graphreduce",
         #packages=["graphreduce"],
         packages = setuptools.find_packages(exclude=[ "docs", "examples" ]),
